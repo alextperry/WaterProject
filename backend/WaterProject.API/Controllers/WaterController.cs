72cc9c89f6f7e92c2024065503473d497afff150
alextperry <alextperry@outlook.com>
2025-03-17 15:44:28 -0600

    Initial commit

---
+++ backend/WaterProject.API/Controllers/WaterController.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WaterProject.API.Data;
+
+namespace WaterProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WaterController : ControllerBase
+    {
+        private WaterDbContext _waterContext;
+
+        public WaterController(WaterDbContext temp) 
+        {
+            _waterContext = temp;
+        }
+
+        [HttpGet("AllProjects")]
+        public IEnumerable<Project> Get()
+        {
+            var list = _waterContext.Projects.ToList();
+
+            return list;
+        }
+
+        //[HttpGet("FunctionalProjects")]
+        public IEnumerable<Project> GetFunctionalProjects()
+        {
+            var list = _waterContext.Projects.Where(p => p.ProjectFunctionalityStatus == "Functional").ToList();
+
+            return list;
+        }
+
+    }
+}
