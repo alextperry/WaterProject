3cb1d186b74ce2efefef078d2e663e04a18f0706
alextperry <alextperry@outlook.com>
2025-03-24 19:30:34 -0600

    gitignore

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import ProjectList from './ProjectList'
+import CookieConsent from "react-cookie-consent";
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +10,8 @@ function App() {
   return (
     <>
       <ProjectList />
+      <CookieConsent>This website uses cookies to enhance the user experience.</CookieConsent>
+
     </>
   )
 }
